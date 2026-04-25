195840c12d13082c9e0997e80ecbbfadc5b887c4
AnkurKartik <ankur.goswami2new@gmail.com>
2026-04-25 12:12:21 +0530

    github actions setup

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
-import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import os
+import pytz
+
+FLAG_FILE = "done.txt"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -10,10 +12,6 @@ CHAT_ID = os.getenv("CHAT_ID")
 URL = "https://results.puexam.in"
 
 TARGET_TEXT = "Master of Engineering Civil Engineering (Construction Technology and Management) 7th Spell Examination, 2025"
-
-CHECK_INTERVAL = 300
-START_TIME = "09:30"
-END_TIME = "20:00"
 
 
 def send_message(text):
@@ -27,9 +25,12 @@ def send_document(file_url):
 
 
 def is_within_time():
-    now = datetime.now().time()
-    start = datetime.strptime(START_TIME, "%H:%M").time()
-    end = datetime.strptime(END_TIME, "%H:%M").time()
+    india = pytz.timezone("Asia/Kolkata")
+    now = datetime.now(india).time()
+
+    start = datetime.strptime("09:30", "%H:%M").time()
+    end = datetime.strptime("20:00", "%H:%M").time()
+
     return start <= now <= end
 
 
@@ -37,9 +38,10 @@ def check_result():
     print("Checking...")
 
     try:
-        response = requests.get(URL, timeout=10)
-        soup = BeautifulSoup(response.text, "html.parser")
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(URL, headers=headers, timeout=10)
 
+        soup = BeautifulSoup(response.text, "html.parser")
         links = soup.find_all("a")
 
         for link in links:
@@ -65,15 +67,7 @@ def check_result():
 
 
 if __name__ == "__main__":
-    send_message("🤖 Bot started")
-
-    while True:
-        if is_within_time():
-            found = check_result()
-            if found:
-                send_message("✅ Done")
-                break
-        else:
-            print("Outside time")
-
-        time.sleep(CHECK_INTERVAL)
+    if is_within_time():
+        check_result()
+    else:
+        print("Outside time")
